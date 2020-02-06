2969e4e4752da1fa855c8472968d926491a3269d
Blake-Jarsky <55511352+Blake-Jarsky@users.noreply.github.com>
2020-02-06 12:44:54 -0800

    Add files via upload

---
+++ angular-interview/src/app/test/test.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '[app-test]',
+  template: `<div>
+              Inline template
+            </div>`,
+  styleUrls: ['./test.component.scss']
+})
+export class TestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
